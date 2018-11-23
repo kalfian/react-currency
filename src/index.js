56e424c44878d5bc99b01502ffe944a369210bc7
kukuh alfian hanif <kukuhalf@gmail.com>
2018-11-23 16:13:20 +0700

    Router

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const App = () => {
     // const title = "React Coin";
 
     return (
-        <BrowserRouter basename="https://kalfian.github.io/react-currency/">
+        <BrowserRouter basename="react-currency/">
         <div>
             <Header/>
 
