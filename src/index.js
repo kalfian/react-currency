3a3170b5a3d1482ecc79bcbc20bba5f61be7f8d1
kukuh alfian hanif <kukuhalf@gmail.com>
2018-12-14 14:48:09 +0700

    Fix Basename

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const App = () => {
     // const title = "React Coin";
 
     return (
-        <BrowserRouter>
+        <BrowserRouter basename="/react-currency/">
         <div>
             <Header/>
 
